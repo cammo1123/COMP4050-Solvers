15d3b0f2927f5735fcb75ee9751ccb7150a7a47f
Cameron Youell <cameronyouell@gmail.com>
2026-08-12 20:11:20 +1000

    refactor: clean up main function and improve output formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
-#include <cstdlib>
 #include <iostream>
+#include <memory>
 
+#include "solve_generated.h"
 #include "solvers.h"
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-	std::cout << solvers::info() << std::endl;
+	std::cout << solvers::info() << '\n';
 
-	if (argc > 1) {
-		double value = std::strtod(argv[1], nullptr);
-		std::cout << solvers::doubleValue(value) << std::endl;
+	solver::SolveRequestT solve;
+
+	for (auto i = 0; i < 100; i++) {
+		auto box = std::make_unique<solver::BoxTypeT>();
+		box->reference = "HELLO";
+		box->width = i * 10;
+		box->length = i * 10;
+		box->depth = i * 10;
+		solve.boxes.push_back(std::move(box));
 	}
+
+	for (auto i = 0; i < 1000; i++) {
+		auto item = std::make_unique<solver::ItemTypeT>();
+		item->item_code = "HELLO";
+		item->item_reference = "HELLO";
+		item->width = i * 1;
+		item->length = i * 1;
+		item->depth = i * 1;
+		solve.items.push_back(std::move(item));
+	}
+
+	auto res = solvers::solve(solve);
+	std::cout << res.boxes.size() << '\n';
+	std::cout << res.items.size() << '\n';
 
 	return 0;
 }
