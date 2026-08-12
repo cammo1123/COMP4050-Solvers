81b573dc72a7d86640d139a7f7750a3b550ac3f5
Cameron Youell <cameronyouell@gmail.com>
2026-08-12 11:43:33 +1000

    feat: update package structure and add TypeScript support with new build configuration

--- node/src/addon.ts
+++ node/src/addon.ts
@@ -1,11 +1,7 @@
 import native from "../index.cjs";
 
-import {
-	decodeResponse,
-	encodeRequest,
-	type DoubleValueRequest,
-	type DoubleValueResponse,
-} from "./gen/doublevalue_translation.js";
+import { decodeResponse, encodeRequest, type DoubleValueRequest, type DoubleValueResponse } from "./gen/doublevalue_translation.js";
+export type { ConfigT, DataT, DoubleValueRequest, DoubleValueResponse } from "./gen/doublevalue_translation.js";
 
 export function doubleValue(request: DoubleValueRequest): DoubleValueResponse {
 	return decodeResponse(native.doubleValue(Buffer.from(encodeRequest(request))));
@@ -14,10 +10,3 @@ export function doubleValue(request: DoubleValueRequest): DoubleValueResponse {
 export function hello(): string {
 	return native.hello();
 }
-
-export type {
-	ConfigT,
-	DataT,
-	DoubleValueRequest,
-	DoubleValueResponse,
-} from "./gen/doublevalue_translation.js";
