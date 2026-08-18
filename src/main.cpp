94ba2ae587979151e7bd840f5ccac646c01ac6bf
Cameron Youell <cameronyouell@gmail.com>
2026-08-18 23:34:57 +1000

    main: Update `main.cpp`

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#include <cstddef>
+#include <cstdio>
 #include <iostream>
+#include <optional>
+#include <stdio.h>
 
 #include "solve_domain_generated.h"
 #include "solvers.h"
@@ -9,30 +13,75 @@ int main()
 {
 	std::cout << solvers::info() << '\n';
 
-	SolveRequest solve;
+	SolveRequest solve = { };
 
-	for (auto i = 0; i < 100; i++) {
-		BoxType box;
-		box.reference = "HELLO";
-		box.width = i * 10;
-		box.length = i * 10;
-		box.depth = i * 10;
-		solve.boxes.push_back(std::move(box));
+	solve.boxes.push_back({
+		.reference = "Small",
+		.width = 10.0f,
+		.length = 10.0f,
+		.depth = 10.0f,
+
+		.maximum_boxes = 2,
+	});
+
+	solve.boxes.push_back({
+		.reference = "Medium",
+		.width = 50.0f,
+		.length = 50.0f,
+		.depth = 50.0f,
+	});
+
+	solve.boxes.push_back({
+		.reference = "Large",
+		.width = 100.0f,
+		.length = 100.0f,
+		.depth = 100.0f,
+	});
+
+	for (auto i = 1; i <= 65; i++) {
+		solve.items.push_back({
+			.item_code = std::string("S") + std::to_string(i),
+			.item_reference = std::string("Small") + std::to_string(i),
+			.width = 1,
+			.length = 1,
+			.depth = 1,
+			.weight = 1,
+		});
 	}
 
-	for (auto i = 0; i < 1000; i++) {
-		ItemType item;
-		item.item_code = "HELLO";
-		item.item_reference = "HELLO";
-		item.width = i;
-		item.length = i;
-		item.depth = i;
-		solve.items.push_back(std::move(item));
+	for (auto i = 1; i <= 25; i++) {
+		solve.items.push_back({
+			.item_code = std::string("S") + std::to_string(i),
+			.item_reference = std::string("Small") + std::to_string(i),
+			.width = 10,
+			.length = 10,
+			.depth = 10,
+			.weight = 10,
+		});
 	}
 
-	auto res = solvers::solve(solve);
-	std::cout << res.boxes.size() << '\n';
-	std::cout << res.items.size() << '\n';
+	for (auto i = 1; i <= 1000; i++) {
+		solve.items.push_back({
+			.item_code = std::string("L") + std::to_string(i),
+			.item_reference = std::string("Large") + std::to_string(i),
+			.width = 50,
+			.length = 50,
+			.depth = 50,
+			.weight = 50,
+		});
+	}
+
+	auto res = solvers::solve(solve, [](size_t done, size_t total) {
+		std::cout << "\rprogress: " << ((static_cast<float>(done) / total) * 100) << "%" << std::flush;
+	});
+	std::cout << "\n";
+
+	int i = 0;
+	for (auto const& result : res.results) {
+		i += result.placements.size();
+	}
+
+	std::cout << "Total: " << i << " items\n";
 
 	return 0;
 }
