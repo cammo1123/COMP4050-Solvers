fcf09d50b3de9fe5fda29919acd898bea20c341c
Cameron Youell <cameronyouell@gmail.com>
2026-09-15 15:19:35 +1000

    feat: add enums for RotationPolicy, SolveAlgorithm, and SolveStrategyOptions

---
+++ node/dist/enums.d.ts
@@ -0,0 +1,4 @@
+export { RotationPolicy } from "./gen/fbs/rotation-policy.js";
+export { SolveAlgorithm } from "./gen/fbs/solve-algorithm.js";
+export { SolveStrategyOptions } from "./gen/fbs/solve-strategy-options.js";
+//# sourceMappingURL=enums.d.ts.map
